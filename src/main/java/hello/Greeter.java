bf0ed19f8785b0c94efc0f6c05314464cb443972
sushanthumane <96003928+sushanthumane@users.noreply.github.com>
2021-12-19 22:13:54 +0530

    update test case

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
   public String sayHello() {
-    return "Hello world- This is Sushant your Trainer !! Hi or Bye hello is the string needed here.";
+    return "Hello world- This is Sushant your Trainer !! Hi or  hello is the string needed here.";
   }
 }
